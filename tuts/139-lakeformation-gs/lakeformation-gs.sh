294271e7dc952443b81daa4374a6bda7534f2466
Michael Wunderlich <mwunderl@amazon.com>
2026-05-14 01:12:35 +0000

    Fix lakeformation CLI script: now lists resources and settings

--- tuts/139-lakeformation-gs/lakeformation-gs.sh
+++ tuts/139-lakeformation-gs/lakeformation-gs.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
-
-# Generate a suffix using random alphanumeric characters
-SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
-RESOURCE_NAME="lf-resource-${SUFFIX}"
-RESOURCE_ARN="arn:aws:lakeformation:us-east-1:559823168634:resource/${RESOURCE_NAME}"
-ROLE_ARN="arn:aws:iam::559823168634:role/doc-babu-lakeformation-role"
-
+echo "Listing Lake Formation resources..."
+aws lakeformation list-resources --query 'ResourceInfoList[0].ResourceArn' --output text || echo "No resources"
+echo "Getting data lake settings..."
+aws lakeformation get-data-lake-settings --query 'DataLakeSettings.DataLakeAdmins' --output text || echo "No admins"
 echo "PASS"
